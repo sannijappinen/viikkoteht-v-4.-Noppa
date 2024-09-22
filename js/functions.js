b66ab6e92d724f78f8831e0a6e2d3a01c27b6f23
sannijappinen <sannjapp@hotmail.com>
2024-09-22 21:10:44 +0300

    työ valmis

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+const diceDiv = document.getElementById('dice');
+function rollDice() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+    const diceImage = `../img/${randomNumber}.png`;
+
+    diceDiv.querySelector('img').src = diceImage;
+}
+
+diceDiv.addEventListener('click', rollDice);
