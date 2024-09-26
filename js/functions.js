89f3941ff3b9a07528dc826482fbc9153155a66a
sannijappinen <sannjapp@hotmail.com>
2024-09-26 14:29:54 +0300

    testailua

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ const diceDiv = document.getElementById('dice');
 function rollDice() {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
     
-    const diceImage = `./img/${randomNumber}.png`;
+    const diceImage = ./img/${randomNumber}.png;
 
     diceDiv.querySelector('img').src = diceImage;
 }
